91b0b50032feb85df016e013d18fb7b84deab276
est22 <anlia.seoul@gmail.com>
2022-10-17 18:57:06 +0900

    done

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    
+    var totalTime = 0 // down
+    var secondsPassed = 0 // up
+    
+    var timer = Timer()
+
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var textLabel: UILabel!
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+
+        timer.invalidate()
+        let hardness = sender.currentTitle! // Soft, Medium, Hard
+        totalTime = eggTimes[hardness]!
+        
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        textLabel.text = hardness
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
+        
+            
+        }
+    
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+            
+        } else {
+            timer.invalidate()
+            textLabel.text = "Done!"
+        }
+       
+
+    }
+    }
+    
 
 
-}
